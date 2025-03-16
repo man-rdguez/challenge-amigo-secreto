140d77a847753900744c5ef76856bd4fd6a9fd4b
man-rdguez <man.rdguez@gmail.com>
2025-03-16 01:56:02 -0600

    Ajustamos codigo de acuerdo a la respectiva tarjeta en Trello

--- app.js
+++ app.js
@@ -15,27 +15,16 @@ function agregarAmigo() {
 
     // Validamos que se haya capturado un nombre
     if ( nombre == '' ) {
-        alert( "Por favor, ingrese un nombre valido." );
+        alert( "Por favor, inserte un nombre." );
     }
     else {
         // Guardamos el nombre del amigo en el array de amigos
         nombres.push( nombre );
 
-        // Obtenemos la referencia a la lista no ordenada, que contiene los nombres de amigos
-        let listaAmigos  = document.getElementById('listaAmigos' );
-
-        // Creamos el elemento list item el cual contendra el nombre proporcionado
-        itemAmigo = document.createElement( "li" );
-
-        // Asignamos el nombre del amigo al list item creado
-        itemAmigo.innerHTML = nombre;
-
-        // Incluimos el nombre del amigo a la lista de amigos que se mostrara debajo del cuadro de texto.
-        listaAmigos.appendChild( itemAmigo );
-
         // Limpiamos el cuadro de texto, de tal forma que si se desea incluir otro nombre en la lista,
         // no sea necesario borrar el nombre anterior manualmente
         amigo.value = '';
     }
+
 }
 
